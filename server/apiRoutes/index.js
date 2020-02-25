4022848bdf0e6c9a8a454f339a90a2a54d9a6f82
Leslie <lesliew1993@gmail.com>
2020-02-25 11:13:17 -0500

    finished creating basic server routes

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -3,4 +3,9 @@ const router = require('express').Router();
 router.use('/users', require('./users'))
 router.use('kittens', require('./kittens'))
 
+router.use( function (req, res, next) {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
 module.exports = router;
